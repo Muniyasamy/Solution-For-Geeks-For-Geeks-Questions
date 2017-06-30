37d8e46612050581b6b23c7eb9e2d579a4315083
Muniyasamy <vrkmuniyasamy95@gmail.com>
2017-06-30 18:34:13 +0530

    Update TempleOffering.c

--- Dynamic Programming/Easy/C/TempleOffering.c
+++ Dynamic Programming/Easy/C/TempleOffering.c
@@ -1,6 +1,5 @@
 /*
 Link : http://practice.geeksforgeeks.org/problems/temple-offerings/0
-
 Consider a devotee wishing to give offerings to temples along a mountain range. The temples are located in a row at different heights. Devotee is very religious and wants to offer each temple at least one offering. If two adjacent temples are at different altitudes, then the temple that is higher up should receive more offerings than the one that is lower down. If two adjacent temples are at the same height, then their offerings relative to each other does not matter. Find the minimum number of offerings to bring.
 
 Input:
@@ -29,34 +28,22 @@ Output:
 Explanation:
 
 Test case 1:
-
 All temples must receive at-least one offering. Now, the second temple is at a higher altitude compared to the first one. Thus it receives one extra offering. The second temple and third temple are at the same height, so we do not need to modify the offerings. Offerings given are therefore: 1, 2, 1 giving a total of 4.
 
 Test case 2:
-
 We can distribute the offerings in the following way, 1, 2, 1, 3, 2, 1. The second temple has to receive more offerings than the first due to its height being higher. The fourth must receive more than the fifth, which in turn must receive more than the sixth. Thus the total becomes 10.
 
-
-
 */
-
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 
 int max( int left , int right ) {
-	
 	if ( left > right )
 	    return left ;
 	return right ;
 }
 
-
 int templeOffer ( int *arr, int n )  {
-	
-	int *dp ;
 	int  i,count = 0 ;
 	int *dp_left = (int *) malloc ( sizeof(int) * n ) ;
 	int *dp_right = (int *) malloc ( sizeof(int) * n ) ;
@@ -82,9 +69,7 @@ int templeOffer ( int *arr, int n )  {
 	return count ;
 }
 
-
 int main() {
-	
 	int t ;
 	int n ;
 	int *arr ;
@@ -98,13 +83,8 @@ int main() {
 		
 		for ( i = 0 ; i < n ; i++ )
 		scanf("%d",&arr[i]) ;
-		
-		
-		printf("%d\n",templeOffer(arr,n));
-		
+		printf("%d\n",templeOffer(arr,n));	
 		free(arr) ;
-		
 	}
-	
 	return 0 ;
 }
